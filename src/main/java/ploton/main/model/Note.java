f5763d6fba0ca4e1760b296aac37ece02461fbdb
Anton P. <made1of1fire@gmail.com>
2024-03-19 19:23:08 +0300

    feat(restapi-model):Added the main class Note, which is the main entity for the application (this is the data displayed to the user, as well as the data that will be stored in MySQL DB and as a cache in Redis).

---
+++ src/main/java/ploton/main/model/Note.java
@@ -0,0 +1,48 @@
+package ploton.main.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String head;
+    private String content;
+    private boolean isCompleted;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+}
