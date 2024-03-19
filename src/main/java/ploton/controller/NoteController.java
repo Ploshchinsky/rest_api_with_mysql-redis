8292ef7f20d1bb583f9c5634bc634bca91aa1346
Anton P. <made1of1fire@gmail.com>
2024-03-19 20:09:25 +0300

    fix(restapi-controller):Updated NoteCotnroller. Added new POST method - save()

--- src/main/java/ploton/controller/NoteController.java
+++ src/main/java/ploton/controller/NoteController.java
@@ -1,16 +1,30 @@
 package ploton.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import ploton.main.model.Note;
+import ploton.main.model.NoteRepository;
 
 @Controller
 @RequestMapping("/notes")
 public class NoteController {
+    @Autowired
+    private static NoteRepository noteRepository;
+
     @GetMapping("")
     @ResponseBody
     public String hello() {
         return "Hello World!";
+    }
+
+    //ADD
+    @PostMapping("/")
+    public ResponseEntity<Note> save(Note note) {
+        return ResponseEntity.ok(noteRepository.save(note));
     }
 }
