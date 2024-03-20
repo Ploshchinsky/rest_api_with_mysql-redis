ecce4c8857aae66b6bbe7ce91410761aa3231b4a
Anton P. <made1of1fire@gmail.com>
2024-03-20 19:47:21 +0300

    fix(service):update RedisCacheRunnable -> added logging inside runnable method

--- src/main/java/ploton/service/RedisCacheRunnable.java
+++ src/main/java/ploton/service/RedisCacheRunnable.java
@@ -1,12 +1,16 @@
 package ploton.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ploton.controller.RedisController;
 import ploton.main.model.Note;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RedisCacheRunnable implements Runnable {
+    private Logger logger = LoggerFactory.getLogger(RedisCacheRunnable.class);
     private List<Note> noteList;
 
     public RedisCacheRunnable(List<Note> noteList) {
@@ -15,6 +19,8 @@ public class RedisCacheRunnable implements Runnable {
 
     @Override
     public void run() {
+        logger.info("Runnable[RedisCache] is started!");
         RedisController.saveInCache(this.noteList);
+        logger.info("Runnable[RedisCache] is finished - List[" + noteList.size() + "] has been save");
     }
 }
