220b207d6f6d4666ee3382cdf03309a0a246ba83
Anton P. <made1of1fire@gmail.com>
2024-03-20 19:57:01 +0300

    fix(service):updated Redis -> Added second mode for Runnable - work with single Note

--- src/main/java/ploton/service/RedisCacheRunnable.java
+++ src/main/java/ploton/service/RedisCacheRunnable.java
@@ -12,15 +12,26 @@ import java.util.List;
 public class RedisCacheRunnable implements Runnable {
     private Logger logger = LoggerFactory.getLogger(RedisCacheRunnable.class);
     private List<Note> noteList;
+    private Note note;
 
     public RedisCacheRunnable(List<Note> noteList) {
         this.noteList = new ArrayList<>(noteList);
     }
 
+    public RedisCacheRunnable(Note note) {
+        this.note = note;
+    }
+
     @Override
     public void run() {
-        logger.info("Runnable[RedisCache] is started!");
-        RedisController.saveInCache(this.noteList);
-        logger.info("Runnable[RedisCache] is finished - List[" + noteList.size() + "] has been save");
+        logger.info("Runnable[RedisCache]: is started!");
+        if (note == null) {
+            logger.info("Runnable[RedisCache]: for Note's List");
+            RedisController.saveInCache(this.noteList);
+        } else {
+            logger.info("Runnable[RedisCache]: for single Note");
+            RedisController.saveInCache(this.note);
+        }
+        logger.info("Runnable[RedisCache]: is finished - List[" + noteList.size() + "] has been save");
     }
 }
